cc2d6e70f03954fddb3fc6309978b0f45bdb8279
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-09-30 13:31:38 -0500

    created Tax Calculation class with corresponding methods and refactored code

--- ShoppingCart.cs
+++ ShoppingCart.cs
@@ -9,10 +9,43 @@ namespace SalesTaxesConsoleApp
     public class ShoppingCart : List<Item>
     {
         List<Item> shoppingCartItems = new List<Item>();
-        
+
         public void AddItemToShoppingCart(Item item)
         {
-            shoppingCartItems.Add(item);
+            bool containsItem = shoppingCartItems.Any(cartItem => cartItem.ItemName == item.ItemName);
+            if (containsItem)
+            {
+                int index = 0;
+                foreach (var cartItem in shoppingCartItems)
+                {
+                    if (cartItem.ItemName == item.ItemName)
+                    {
+                        index = shoppingCartItems.IndexOf(cartItem);
+                    }
+                }
+                shoppingCartItems.RemoveAt(index);
+                UpdateItemFromShoppingCart(item);
+            }
+            else
+            {
+                shoppingCartItems.Add(item);
+            }
+        }
+
+        public void UpdateItemFromShoppingCart(Item item)
+        {
+            int newItemQuantity = item.Quantity + 1;
+            string newTotalPrice = item.TotalPrice + new CurrencyToNumberConverter().ConvertFromCurrency(item.ItemRetailPrice);
+            Double newPriceAfterTax = (item.TotalPriceAfterTax + item.TotalPriceAfterTax);
+            Double accruedImportTax = (item.AccruedImportTax + item.ImportTax);
+            Double accruedSalesTax = (item.AccruedSalesTax + item.SalesTax);
+            Double accruedOverallTax = (item.AccruedOverallTax + item.AddedTax);
+            Item updatedItem = new Item
+            (
+                item.ItemName, item.ItemRetailPrice, item.IsImport, item.Type, item.ImportTax, item.SalesTax, item.AddedTax, newPriceAfterTax, 
+                accruedImportTax, accruedSalesTax, accruedOverallTax, newTotalPrice, newItemQuantity
+            );
+            shoppingCartItems.Add(updatedItem);
         }
 
         public List<Item> GetItemsFromShoppingCart()
